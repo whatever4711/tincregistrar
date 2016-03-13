c545cc0a4e0313b405cd71741bb4cd4382fa00db
eieste <github@ultraapp.de>
2016-03-13 13:31:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sleep 5
 docker exec tincregistrator_web_1 python manage.py makemigrations
 docker exec tincregistrator_web_1 python manage.py migrate
 docker-compose restart web
-docker exec tincregistrator_web_1 /bin/bash -c "python manage.py createsuperuser --username admin --email admin@example.com"
+docker exec tincregistrator_web_1 python manage.py createsuperuser --username "admin" --email "admin@example.com"
