5d93da8320b5e5bf1459105f780d85e94cb44478
eieste <github@ultraapp.de>
2016-03-13 13:04:47 +0100

    Update urls.py

--- tinc/config/urls.py
+++ tinc/config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.site.site_header = 'tinc Registrator'
+
 urlpatterns = [
     url(r'^regService/', include('regService.urls')),
     url(r'^admin/', admin.site.urls, name='admin_site'),
