f936a3e9c9a47670ffa84fd7a8a61d41d1581fc6
eieste <github@ultraapp.de>
2016-03-13 14:06:21 +0100

    Update admin.py

--- tinc/regService/admin.py
+++ tinc/regService/admin.py
@@ -6,8 +6,11 @@ from .models import Node
 from .models import Network
 
 class NodeAdmin(admin.ModelAdmin):
-  list_filter = ('network', 'hostname', 'public_IP', 'config_IP', 'private_IP',)
-  list_display = ('hostname', 'public_IP', 'config_IP', 'private_IP', )
+  list_filter = ('network_name', 'hostname', 'public_IP', 'config_IP', 'private_IP',)
+  list_display = ('hostname', 'network_name', 'public_IP', 'config_IP', 'private_IP', )
+
+  def network_name(self, obj):
+        return obj.network_set.netname
 
 admin.site.register(Node, NodeAdmin)
 
