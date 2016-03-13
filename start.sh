5ebd0467d637b765fe80feaa3cfcd1a6b218fa86
eieste <github@ultraapp.de>
2016-03-13 13:25:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sleep 5
 docker exec tincregistrator_web_1 python manage.py makemigrations
 docker exec tincregistrator_web_1 python manage.py migrate
 docker-compose restart web
-docker exec tincregistrator_web_1 /bin/bash -c "echo \"from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')\" | python manage.py shell"
+docker exec tincregistrator_web_1 python manage.py createsuperuser --username admin --email admin@example.com
