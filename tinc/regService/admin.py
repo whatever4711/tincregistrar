43fd359995c624b0128f7b4c577980539e5f2ae7
eieste <github@ultraapp.de>
2016-03-13 14:07:24 +0100

    Update admin.py

--- tinc/regService/admin.py
+++ tinc/regService/admin.py
@@ -10,7 +10,7 @@ class NodeAdmin(admin.ModelAdmin):
   list_display = ('hostname', 'network_name', 'public_IP', 'config_IP', 'private_IP', )
 
   def network_name(self, obj):
-        return obj.network_set.netname
+        return obj.network.netname
 
 admin.site.register(Node, NodeAdmin)
 
