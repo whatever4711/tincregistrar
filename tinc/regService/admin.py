63d136c6b54643ce15d5490e0a642685177c9d80
eieste <github@ultraapp.de>
2016-03-13 14:01:01 +0100

    Update admin.py

--- tinc/regService/admin.py
+++ tinc/regService/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from .models import Node
 
 class NodeAdmin(admin.ModelAdmin):
-  list_display
   list_filter = ('network', 'hostname', 'public_IP', 'config_IP', 'private_IP',)
   list_display = ('hostname', 'public_IP', 'config_IP', 'private_IP', )
 
